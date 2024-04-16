1e89988b74de1a52c5f89155b2e3a1ea0ed1efae
AnnikDev <a_kvernadze@cu.edu.ge>
2024-04-16 20:30:18 +0400

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,76 @@
 import { useState } from "react";
+import "./index.css";
 import "./App.css";
+import data from "./data.json";
 
 function App() {
-  return <></>;
+  const [notifications, setNotifications] = useState(data);
+  return (
+    <>
+      <div className="notificationsContainer">
+        <header>
+          <div className="headerTitle">
+            <h2>Notifications </h2>
+            <span className="notificationNum">3</span>
+          </div>
+          <p>Mark all as read</p>
+        </header>
+        <main>
+          {notifications.map((notification) => {
+            return (
+              <div key={notification.id} className="notification">
+                <img
+                  className="profilePic"
+                  src={notification.profilePic}
+                  alt=""
+                />
+                <div className="fullDetail">
+                  <div className="notificationDetails">
+                    <p>
+                      <span className="username">{notification.username}</span>
+                      <span className="action">{notification.action}</span>{" "}
+                      {notification.post ? (
+                        <span className="post">{notification.post}</span>
+                      ) : null}
+                      {notification.groupName ? (
+                      <span className="groupName">
+                        {notification.groupName}
+                      </span>
+                    ) : null}
+                    </p>
+                    {/* <span className="username">{notification.username}</span> */}
+                    {/* <span className="action">{notification.action}</span> */}
+                    {/* {notification.post ? (
+                      <span className="post">{notification.post}</span>
+                    ) : null} */}
+                    {/* {notification.groupName ? (
+                      <span className="groupName">
+                        {notification.groupName}
+                      </span>
+                    ) : null} */}
+                    {!notification.isRead ? (
+                      <div className="redDot"></div>
+                    ) : null}
+                  </div>
+                  <p className="time">{notification.time}</p>
+                  {notification.text ? (
+                    <p className="text">{notification.text}</p>
+                  ) : null}
+                </div>
+                {notification.userPicture ? (
+                  <img
+                    className="userPic"
+                    src={notification.userPicture}
+                    alt=""
+                  />
+                ) : null}
+              </div>
+            );
+          })}
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default App;
